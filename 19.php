fa975bef3bda8d42b92dacc9b3f2ae6befb26deb
legkiy1983 <vovik-l@yandex.ru>
2017-05-23 00:49:08 +0300

    Update 19.php

--- 19.php
+++ 19.php
@@ -1,9 +1,6 @@
 <?php
-$a = '78';
-$b = 78;
-if($a === $b){
-    var_dump ($a.' equal'.$b);
-}
-else {
-    var_dump($a.' not equal '.$b);
-}
+include '17.php';
+echo "<br>";
+var_dump ($a);
+echo "<br>";
+var_dump ($b);
